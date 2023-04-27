5b4b6b42f71d722c0ae47574d7488aca6f5294c3
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-27 13:41:14 +0300

    fix:add codewars

--- index.js
+++ index.js
@@ -1,24 +1,169 @@
-let str = 'hello';
-console.log(str.toUpperCase());
-console.log(str.toLowerCase())
-console.log(str.toLocaleLowerCase())
-console.log(str.toLocaleUpperCase())
-console.log(str.toString())
+// let str = 'hello';
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase())
+// console.log(str.toLocaleLowerCase())
+// console.log(str.toLocaleUpperCase())
+// console.log(str.toString())
 
 
-let number = 12.98765;
-console.log(number.toFixed())
-console.log(number.toLocaleString(2))
-console.log(number.toString())
-console.log(number.toExponential())
+// let number = 12.98765;
+// console.log(number.toFixed())
+// console.log(number.toLocaleString(2))
+// console.log(number.toString())
+// console.log(number.toExponential())
 
-// let capitalizeWord = 'word';
-// console.log(capitalizeWord.slice(1).toUpperCase)
-function capitalizeWord(word) {
-    return word[0].toUpperCase() + word.slice(1);
-    //   return word[0].toUpperCase() + word.substring(1);
-  }
-  console.log(capitalizeWord('helloword ggg fff'))
+// // let capitalizeWord = 'word';
+// // console.log(capitalizeWord.slice(1).toUpperCase)
+// function capitalizeWord(word) {
+//     return word[0].toUpperCase() + word.slice(1);
+//     //   return word[0].toUpperCase() + word.substring(1);
+//   }
+//   console.log(capitalizeWord('helloword ggg fff'))
 
+// codewars doesn't work 
+//   function century(year) {
+//    const i = year;
+//    if  (i >= 1  && i <= 100) {
+//         console.log('1')
+//    } else if (i >= 101  && i <= 200) {
+        
+//         console.log('2')
+//    } else if (i >= 201  && i <= 300) {
+        
+//         console.log('3')
+//    } else if (i >= 301  && i <= 400) {
+        
+//         console.log('4')
+//    } else if (i >= 401  && i <= 500) {
+        
+//         console.log('5')
+//    } else if (i >= 501  && i <= 600) {
+        
+//         console.log('6')
+//    } else if (i >= 601  && i <= 700) {
+        
+//         console.log('7')
+//    } else if (i >= 701  && i <= 800) {
+        
+//         console.log('8')
+//    } else if (i >= 801  && i <= 900) {
+        
+//         console.log('9')
+//    } else if (i >= 901  && i <= 1000) {
+       
+//         console.log('10')
+//    } else if (i >= 1001  && i <= 1100) {
+        
+//         console.log('11')
+//    } else if (i >= 1101  && i <= 1200) {
+        
+//     console.log('12')
+// } else if (i >= 1201  && i <= 1300) {
+        
+//     console.log('13')
+// } else if (i >= 1301  && i <= 1400) {
+        
+//     console.log('14')
+// } else if (i >= 1401  && i <= 1500) {
+        
+//     console.log('15')
+// } else if (i >= 1501  && i <= 1600) {
+        
+//     console.log('16')
+// } else if (i >= 1601  && i <= 1700) {
+        
+//     console.log('17')
+// } else if (i >= 1701  && i <= 1800) {
+        
+//     console.log('18')
+// } else if (i >= 1801  && i <= 1900) {
+        
+//     console.log('19')
+// } else if (i >= 1901  && i <= 2000) {
+        
+//     console.log('20')
+// } else if (i >= 2001  && i <= 2100) {
+        
+//     console.log('21')
+// } else if (i >= 2001  && i <= 2200) {
+        
+//     console.log('22')
+// }
+    
+//   }
+ 
+// century(1705)
 
+// codewars works
+// function century(year) {
+//    return Math.ceil(year / 100);
+    
+// }
+// console.log(century(1905))
   
+//число в строку
+
+// function numberToString(num) {
+   
+//     return num.toString()
+//   }
+//   console.log(numberToString(46))
+
+// строка в число не раб в кодварс
+//   const stringToNumber = function(num){
+//     return num;
+//   }
+//   console.log(stringToNumber('8'))
+
+
+// строка в число
+//   const stringToNumber = Number;
+   
+// console.log(Number('78'))
+
+// const stringToNumber = function(str){
+//         return +str;
+//       }
+//       console.log(stringToNumber('8765'))
+
+
+// бинарный формат
+// function toBinary(n){
+    
+//     return +n.toString(2);
+//   }
+  
+   
+//   console.log((89).toString(2))
+
+
+
+// not codewars, without return
+
+// function evenOrOdd(num) {
+    
+//         if (num % 2 === 0) {
+//        console.log('even')
+//     } else {
+//         console.log('odd')
+//     }
+// }
+//   evenOrOdd(1)
+//   
+
+
+// function evenOrOdd(num) {
+       
+// return (num % 2 === 0) ? "Even" : "Odd"
+    
+// }
+
+// // function evenOrOdd(num) {
+    
+// //     if (num % 2 === 0) {
+// //     return "Even";
+// //  } else {
+// //      return "Odd";
+// //  }
+// // }
+// console.log(evenOrOdd(1))
